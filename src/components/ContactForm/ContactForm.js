a1026c018647cdb5433d6263b4401611896fbf33
Aleksanrda Sluchevska <alaska3a@gmail.com>
2021-09-15 20:44:51 +0100

    hooks contact form

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,72 +1,12 @@
 import { useState } from 'react';
 import { LabelInput, Input, Button } from './ContactForm.styled';
 
-// class FormInput extends Component {
-//   state = {
-//     name: '',
-//     number: '',
-//   };
-
-//   handleChange = e => {
-//     this.setState({
-//       [e.currentTarget.name]: e.currentTarget.value,
-//     });
-//   };
-
-//   handleSubmit = e => {
-//     e.preventDefault();
-//     this.props.onSubmit(this.state);
-//     this.reset();
-//   };
-
-//   reset = () => {
-//     this.setState({
-//       name: '',
-//       number: '',
-//     });
-//   };
-
-//   render() {
-//     return (
-//       <div>
-//         <form onSubmit={this.handleSubmit}>
-//           <LabelInput>Name</LabelInput>
-//           <Input
-//             type="text"
-//             name="name"
-//             value={this.state.name}
-//             onChange={this.handleChange}
-//             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-//             title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
-//             required
-//           />
-
-//           <LabelInput>Number</LabelInput>
-//           <Input
-//             type="tel"
-//             name="number"
-//             value={this.state.number}
-//             onChange={this.handleChange}
-//             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-//             title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
-//             required
-//           />
-
-//           <Button type="submit">Add contact</Button>
-//         </form>
-//       </div>
-//     );
-//   }
-// }
-
-// export default FormInput;
-
-export default function ContactForm() {
+export default function ContactForm({onSubmit}) {
   const [name, setName] = useState('');
   const [number, setNumber] = useState('');
 
   const handleChange = e => {
-    switch (e.target.name){
+    switch (e.target.name) {
       case 'name':
         setName(e.target.value);
         break;
@@ -74,15 +14,22 @@ export default function ContactForm() {
         setNumber(e.target.value);
         break;
       default:
-        return
+        return;
     }
   };
-  // const[name, setName] = useLocalStorage('name', '')
-  // const[number, setNumber] = useLocalStorage('number', '')
 
+  const reset = () => {
+    this.setState({
+      setName: '',
+      setNumber: '',
+    });
+  };
 
   const handleSubmit = e => {
-    console.log(e);
+    e.preventDefault();
+
+    onSubmit(name, number);
+    reset();
   };
   return (
     <div>
